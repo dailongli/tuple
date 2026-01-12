fb7ea71267de3f1eb0ebd5585fcca1acd9bc5fe8
Dailong Li <dailongli@hotmail.com>
2026-01-12 17:53:09 +0700

    update

--- ext/tuple.c
+++ ext/tuple.c
@@ -69,42 +69,26 @@ static VALUE tuple_dump_internal(VALUE tuple) {
     for (i = 0; i < RARRAY_LEN(tuple); i++) {
         item = RARRAY_PTR(tuple)[i];
         header[0] = header[1] = header[2] = header[3] = 0;
-         if (FIXNUM_P(item)) {
-            int64_t fixnum;
-            uint64_t ufixnum;
 
-            fixnum = NUM2LL(item);
-
-            if (fixnum >= 0) {
-                sign = 1;
-                ufixnum = (uint64_t)fixnum;
-            } else {
-                sign = 0;
-                ufixnum = (uint64_t)(-(fixnum + 1)) + 1;
-            }
-
-            len = ufixnum > UINT_MAX ? 2 : 1;
-
+        if (RB_INTEGER_TYPE_P(item)) {
+        //if (FIXNUM_P(item)) {
+            fixnum = FIX2LONG(item);
+            sign = (fixnum >= 0);
+            if (!sign) fixnum = -fixnum;
+            len = fixnum > UINT_MAX ? 2 : 1;
             header[2] = sign ? INTP_SORT : INTN_SORT;
             header[3] = sign ? len : UCHAR_MAX - len;
-            rb_str_cat(data, (char*)&header, sizeof(header));
+            rb_str_cat(data, (char*)&header, sizeof(header));      
 
-            ufixnum = (uint64_t)fixnum;
-
-            /* high 32 bits */
             if (len == 2) {
-                digit = split64(ufixnum, 1);
-                if (!sign) digit = UINT32_MAX - digit;
-                digit = htonl(digit);
-                rb_str_cat(data, (char *)&digit, sizeof(digit));
+            digit = split64(fixnum, 1);
+            digit = htonl(sign ? digit : UINT_MAX - digit);
+            rb_str_cat(data, (char*)&digit, sizeof(digit));
             }
-
-            /* low 32 bits */
-            digit = split64(ufixnum, 0);
-            if (!sign) digit = UINT32_MAX - digit;
-            digit = htonl(digit);
-            rb_str_cat(data, (char *)&digit, sizeof(digit));
-        }
+            digit = split64(fixnum, 0);
+            digit = htonl(sign ? digit : UINT_MAX - digit);
+            rb_str_cat(data, (char*)&digit, sizeof(digit));
+        }         
 
         // if (FIXNUM_P(item)) {   
         //     //fixnum = FIX2LONG(item);
