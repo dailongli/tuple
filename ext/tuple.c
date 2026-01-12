521748a06e4c7a8c1eded60317d4cce6f22fea6a
Dailong Li <dailongli@hotmail.com>
2026-01-12 16:31:48 +0700

    update

--- ext/tuple.c
+++ ext/tuple.c
@@ -312,5 +312,5 @@ void Init_tuple() {
 
     mTuple = rb_define_module("Tuple");
     rb_define_module_function(mTuple, "dump", RUBY_METHOD_FUNC(tuple_dump), 1);
-    rb_define_module_function(mTuple, "load", tuple_load, 1);
+    rb_define_module_function(mTuple, "load", RUBY_METHOD_FUNC(tuple_load), 1);
 }
